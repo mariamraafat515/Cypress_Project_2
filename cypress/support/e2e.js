9a7454ce4b64f2917d3ef941b90ccc9c069312d1
Mariam <YOUR_EMAIL>
2026-05-16 01:32:42 +0300

    Remove Cloudflare blocking wait

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -4,8 +4,4 @@ beforeEach(() => {
     cy.visit('/', {
         failOnStatusCode: false
     })
-
-    cy.contains('Practice Software Testing', {
-        timeout: 60000
-    }).should('be.visible')
 })
