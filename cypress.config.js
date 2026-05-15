90675af8fb7dbb12ce90be9290f805f80d47710c
Mariam <YOUR_EMAIL>
2026-05-15 23:11:46 +0300

    Fix Cypress config syntax

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require("cypress");
 
+module.exports = defineConfig({
+  retries: 2,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 
 const {
@@ -10,9 +20,6 @@ const {
   createEsbuildPlugin,
 } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
-module.exports = defineConfig({
-  retries: 2,
-});
   e2e: {
 
     specPattern: "cypress/e2e/features/*.feature",
